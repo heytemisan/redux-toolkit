154420d8c892b489ace41739f6eeb002a888a455
Heytemisan <temyicy@gmail.com>
2022-02-25 23:36:55 +0100

    feat: styled App.css

--- react-redux/src/App.tsx
+++ react-redux/src/App.tsx
@@ -10,32 +10,37 @@ function App() {
   const count  = useSelector((state: RootState) => state.counter.count);
   const dispatch = useDispatch(); //call action from the reducer
   return (
-    <div className='App'>
+    <div className='app'>
       <h1>Redux</h1>
-      <div>
-        <button
-          className='btn'
-          aria-label="Increment value"
-          onClick={()=> dispatch(increment())}
-        >
-          Increment (+)
-        </button>
-        <span className='count'>{count}</span>
-        <button
-          className='btn'
-          aria-label="Decrement value"
-          onClick={()=> dispatch(decrement())}
-        >
-          Decrement (-)
-        </button>
+      <div className="app__container">
+        <div className="first">
+          <button
+            className='btn'
+            aria-label="Increment value"
+            onClick={()=> dispatch(increment())}
+          >
+            Increment (+)
+          </button>
+          <span className='count'>{count}</span>
+          <button
+            className='btn'
+            aria-label="Decrement value"
+            onClick={()=> dispatch(decrement())}
+          >
+            Decrement (-)
+          </button>
+        </div>
+        <div className="second">
+          <button
+            className='btnAmt'
+            aria-label="incrementByAmount"
+            onClick={()=> dispatch(incrementByAmount(20))}
+          >
+            Increment by 20
+          </button>
+        </div>
       </div>
-      <button
-          className='btnAmt'
-          aria-label="incrementByAmount"
-          onClick={()=> dispatch(incrementByAmount(20))}
-        >
-          Increment by 20
-        </button>
+      
     </div>
   );
 }
